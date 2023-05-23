718e21a95063065d3f11e7e05b7f3b5543646a95
Ilhamsyahputra <ilham@indonesiadigital.co.id>
2023-05-23 15:40:07 +0700

    5/23/2023 - Refactoring 4

--- components/organism/SideBar/MenuItem.tsx
+++ components/organism/SideBar/MenuItem.tsx
@@ -1,24 +1,39 @@
-import Image from 'next/image';
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import cx from 'classnames';
+import Link from "next/link";
 
 interface MenuItemProps {
-    title : string;
-    link : string;
-    src : string;
+  title: string;
+  link: string;
+  src: string;
+  active?: boolean;
 }
 
-export default function MenuItem(props: MenuItemProps) {
-    const {title} = props;
-    const {link} = props;
-    const {src} = props;
+export default function MenuItem( props: Partial<MenuItemProps>) {
+  const { title, link, src, active} = props;
+
+  const classItem = cx({
+    item : true,
+    'mb-30' : true,
+    'active' : active,
+  });
+
   return (
     <div>
-      <div className="item active mb-30">
-        <Image src={`/icon/ic-menu-${src}`} alt={'icon'} width={25} height={25}/>
+      <div className={classItem}>
+        <div className="me-3">
+          <Image
+            src={`/icon/ic-menu-${src}.svg`}
+            alt={"icon"}
+            width={25}
+            height={25}
+          />
+        </div>
         <p className="item-title m-0">
-          <a href={link} className="text-lg text-decoration-none">
+          <Link href={`${link}`} className="text-lg text-decoration-none">
             {title}
-          </a>
+          </Link>
         </p>
       </div>
     </div>
--- components/organism/SideBar/index.tsx
+++ components/organism/SideBar/index.tsx
@@ -10,7 +10,7 @@ export default function SideBar() {
           <div className="content pt-50 pb-30 ps-30">
             <Profile />
             <div className="menus">
-              <MenuItem title={'Overview'} link={'/overview'} src={'overview'} />
+              <MenuItem title={'Overview'} link={'/overview'} src={'overview'} active/>
               <MenuItem title={'Transactions'} link={'/transactions'} src={'transactions'} />
               <MenuItem title={'Messages'} link={'/messages'} src={'messages'} />
               <MenuItem title={'Card'} link={'/card'} src={'card'} />
