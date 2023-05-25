c3d86be792f868d669feee15a17b68de012c6824
Ilhamsyahputra <ilham@indonesiadigital.co.id>
2023-05-25 12:01:09 +0700

    From New Branch

--- pages/member/index.tsx
+++ pages/member/index.tsx
@@ -4,6 +4,7 @@ import SideBar from "@/components/organism/SideBar";
 export default function Member() {
   return (
     <>
+    {/* Testing */}
       <section className="overview overflow-auto">
         <SideBar activeMenu={"overview"} />
         <OverviewContent />
