abf545196ef0b723a97d836e36f5aa0568737704
Ilhamsyahputra <ilham@indonesiadigital.co.id>
2023-05-25 12:06:09 +0700

    Test Branch 2

--- pages/member/index.tsx
+++ pages/member/index.tsx
@@ -4,11 +4,12 @@ import SideBar from "@/components/organism/SideBar";
 export default function Member() {
   return (
     <>
-    {/* Testing */}
+    {/* Testing Branch 2*/}
       <section className="overview overflow-auto">
         <SideBar activeMenu={"overview"} />
         <OverviewContent />
       </section>
+      {/* Testing branch 2 */}
     </>
   );
 }
